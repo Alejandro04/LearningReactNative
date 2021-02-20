1378cc09c9ebdf0000fb67cb293b548b9dce25a9
Alejandro Roa <alejandro.roa@globalesm.com>
2021-02-20 10:17:37 -0400

    init

---
+++ src/Hello.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { View, Text, Image, StyleSheet } from 'react-native'
+
+const Hello = (props) => {
+    return (
+        <View style={props.styleContainer}>
+            <Text style={props.styleTitle}>HELLO WORLD</Text>
+            <View style={styles.viewContainer}>
+                <Image source={{ uri: 'https://randomuser.me/portraits/men/10.jpg' }}
+                    style={styles.img} />
+            </View>
+            <View style={styles.viewContainer}>
+                <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+                <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
+                <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewContainer: {
+        flex: 1, 
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center', 
+        marginTop: 20
+    },
+    img: {
+        width: 100,
+        height: 100,
+        borderRadius: 100 / 2,
+    }
+})
+
+
+export default Hello;
